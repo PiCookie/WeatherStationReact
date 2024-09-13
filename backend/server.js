98c0da25ded322a57878437f1e3ad6e0a210c03c
Lukas Erfkämper <l.erfkaemper@gmx.de>
2024-09-13 20:25:02 +0200

    adds api to receive weather dates and put backs  database creation

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(cors());
+app.use(express.json());
+// Middleware zum Parsen von URL-kodierten Daten
+app.use(express.urlencoded({ extended: true }));
+
 // Bestimme den richtigen Pfad zur .env-Datei basierend auf der Umgebung
 const envFile = () => {
     switch (process.env.NODE_ENV) {
@@ -19,67 +24,146 @@ const envFile = () => {
             return "../.env"; // Fallback zur allgemeinen .env-Datei
     }
 };
-
 dotenv.config({ path: path.resolve(process.cwd(), envFile()) });
 
+// Port des Servers
 const PORT = process.env.EXPRESS_PORT || 5001;
 
-app.use(cors());
-app.use(express.json());
-
-// TODO: Beispiel-Route, die Temperaturdaten zurückgibt
-/* app.get("/temperaturdaten", (req, res) => {
-    const data = [
-        { temperatur: 22.5, zeitstempel: new Date() },
-        { temperatur: 21.8, zeitstempel: new Date() },
-    ];
-    res.json(data);
-}); */
-
-// PostgreSQL-Datenbankverbindung einrichten (zur Admin-Datenbank oder einer existierenden)
+// PostgreSQL-Datenbankverbindung einrichten
 const pgClient = new Client({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    database: "postgres",
+    database: process.env.DB_NAME,
 });
 
 // Verbindung zum PostgreSQL-Server herstellen
-pgClient.connect();
+pgClient.connect((err) => {
+    if (err) {
+        console.error("Verbindung zum PostgreSQL-Server fehlgeschlagen:", err);
+        return;
+    }
+    console.log("Erfolgreich mit dem PostgreSQL-Server verbunden.");
+});
 
-console.log("Verbinde mit Datenbank...");
+// Fehlerbehandlung für nicht gefundene Routen
+app.use("*", (req, res) => {
+    res.status(404).send("404 - Seite nicht gefunden");
+});
+
+// POST-Route zum Empfangen von Wetterdaten
+app.post("/api/weather", async (req, res) => {
+    // Wetterdaten aus dem Request-Body extrahieren
+
+    const { token, temperature, humidity, pressure, battery } = req.body;
+    console.log("-----------------------------------------------");
+    console.log(
+        `Wetterdaten empfangen: Temperatur: ${temperature}°C, Luftfeuchtigkeit: ${humidity}%, Luftdruck: ${pressure}hPa, Batterie des Geräts: ${battery}%. Token lautet: ${token}`
+    );
+    console.log("-----------------------------------------------");
+
+    // Überprüfe, ob der Token korrekt ist
+    if (token !== process.env.DEVICE_TOKEN) {
+        return res.status(401).send("Token ungültig");
+    }
+
+    // Überprüfe, ob alle Wetterdaten vorhanden sind
+    if (
+        temperature === undefined ||
+        humidity === undefined ||
+        pressure === undefined ||
+        battery === undefined
+    ) {
+        return res.status(400).send("Fehlende Wetterdaten");
+    }
+
+    // Wetterdaten in die Datenbank einfügen
+    /*     try {
+        // Wetterdaten in die Tabelle "device_sensor_values" einfügen
+        const insertQuery = `
+          INSERT INTO device_sensor_values (device_sensor_id, value, created_at) 
+          VALUES ($1, $2, CURRENT_TIMESTAMP), ($1, $3, CURRENT_TIMESTAMP);
+        `;
+
+        // Es wird davon ausgegangen, dass "device_sensor_id" auf den Gerätetyp verweist (z.B. Temperatur- oder Feuchtigkeitssensor)
+        await client.query(insertQuery, [device_id, temperature, humidity]);
+
+        res.status(201).send("Wetterdaten erfolgreich gespeichert");
+    } catch (err) {
+        console.error("Fehler beim Einfügen von Wetterdaten", err);
+        res.status(500).send("Fehler beim Speichern der Wetterdaten");
+    } */
+
+    res.status(201).send(`Wetterdaten erfolgreich gespeichert`);
+});
+
+// Starte den Server
+app.listen(PORT, () => {
+    console.log(
+        `Server läuft auf http://localhost:${PORT} mit folgenden Umgebungsvariablen:${process.env.NODE_ENV}`
+    );
+});
+
+// TODO LE: Datenbank und Tabellen erstellen
+
+//console.log("Verbinde mit Datenbank...");
 
 // Request vorbereiten
-app.post("/create-database", async (req, res) => {
+/* app.post("/create-database", async (req, res) => {
     console.log("create-database");
     const dbName = process.env.DB_NAME; // Name der Datenbank, die du erstellen möchtest
     try {
         // Fehlerhandlying database already exists
-        /* const result = await pgClient.query(
+        const result = await pgClient.query(
             `SELECT 1 FROM pg_database WHERE datname = $1`,
             [dbName]
         );
 
         if (result.rowCount > 0) {
+            // Verbindung zur erstellten Datenbank herstellen
+            pgClient.end(); // Beende die alte Verbindung
+            const newClient = new Client({
+                user: process.env.DB_USER,
+                host: process.env.DB_HOST,
+                database: process.env.DB_NAME,
+                password: process.env.DB_PASSWORD,
+                port: process.env.DB_PORT,
+            });
+            await newClient.connect();
+
             return res
                 .status(400)
                 .send(`Datenbank ${dbName} existiert bereits.`);
-        } */
+        }
 
         // SQL-Befehl zum Erstellen der Datenbank ausführen
         await pgClient.query(`CREATE DATABASE ${dbName};`);
         res.send(`Datenbank ${dbName} erfolgreich erstellt.`);
+
+        // Verbindung zur erstellten Datenbank herstellen
+        pgClient.end(); // Beende die alte Verbindung
+        const newClient = new Client({
+            user: process.env.DB_USER,
+            host: process.env.DB_HOST,
+            database: process.env.DB_NAME,
+            password: process.env.DB_PASSWORD,
+            port: process.env.DB_PORT,
+        });
+        await newClient.connect();
+        console.log(`Mit der neuen Datenbank ${dbName} verbunden.`);
     } catch (err) {
         if (err.code === "42P04") {
             // Fehlercode für "Datenbank existiert bereits"
-            res.status(400).send(`Datenbank ${dbName} existiert bereits.`);
+            //res.status(400).send(`Datenbank ${dbName} existiert bereits.`);
         } else {
             console.error(err);
             res.status(500).send("Fehler beim Erstellen der Datenbank.");
         }
     }
-});
+}); */
+
+/* console.log("Request versenden...");
 
 // Request versenden
 fetch("http://localhost:5005/create-database", {
@@ -89,16 +173,14 @@ fetch("http://localhost:5005/create-database", {
     .then((data) => console.log(data))
     .catch((error) => console.error("Error:", error));
 
-// Starte den Server
-app.listen(PORT, () => {
-    console.log(
-        `Server läuft auf http://localhost:${PORT} mit folgenden Umgebungsvariablen:${process.env.NODE_ENV}`
-    );
-});
+
+
+console.log("Datenbank Tabellen erstellen...");
 
 // TODO:
-/* const sql = fs.readFileSync("./db/schema.sql").toString();
+const sql = fs.readFileSync("./db/schema.sql").toString();
 
-pool.query(sql)
+pgClient
+    .query(sql)
     .then(() => console.log("Schema erfolgreich ausgeführt"))
     .catch((err) => console.error("Fehler beim Ausführen des Schemas:", err)); */
