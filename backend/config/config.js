fe25683f1463f746854474e71c2afa3a22132371
Lukas Erfkämper <l.erfkaemper@gmx.de>
2024-09-13 22:28:12 +0200

    creates database with sequelize-cli db:create

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,10 +1,17 @@
+const env = process.env.NODE_ENV || "development";
+
+require("dotenv").config({
+    path: `${process.cwd()}/.env.${env}`,
+});
+
 module.exports = {
     development: {
-        username: "root",
-        password: null,
-        database: "database_development",
-        host: "127.0.0.1",
-        dialect: "mysql",
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
     },
     test: {
         username: "root",
